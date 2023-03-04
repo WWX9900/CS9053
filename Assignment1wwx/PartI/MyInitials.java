8047ba57757cfbf920998d6f92606c56ce3edd0f
WWX9900 <117238335+WWX9900@users.noreply.github.com>
2023-03-03 23:48:30 -0500

    Add files via upload

---
+++ Assignment1wwx/PartI/MyInitials.java
@@ -0,0 +1,15 @@
+
+public class MyInitials {
+
+	public static void main(String[] args) {
+		System.out.println(
+				  "W   W  X   X  W   W\n"
+				+ "W   W  X   X  W   W\n"
+				+ "W   W   X X   W   W\n"
+				+ "W W W    X    W W W\n"
+				+ "WW WW   X X   WW WW\n"
+				+ "WW WW  X   X  WW WW\n"
+				+ "W   W  X   X  W   W\n"
+				+ "");
+	}
+}
---
+++ Assignment1wwx/PartIII/WireCapacitance.java
@@ -0,0 +1,22 @@
+class WireCapacitance {
+
+	
+	public static void main(String[] arguments) {
+		double absolutePermittivity = 8.85E-12;
+		double initialDistance = .01;
+		double finalDistance = .005;
+		double wireLength = 0.5;
+		double wireRadius = .002053;
+		double topresult = Math.PI * absolutePermittivity * wireLength;
+		double bottomresultinitial = Math.log(initialDistance/(2*wireRadius)+
+				Math.sqrt((Math.pow(initialDistance,2)/(4*Math.pow(wireRadius,2)))-1));
+		double c1 = topresult/bottomresultinitial;
+		double bottomresultfinal = Math.log(finalDistance/(2*wireRadius)+
+		Math.sqrt((Math.pow(finalDistance,2)/(4*Math.pow(wireRadius,2)))-1));
+		double c2 = topresult/bottomresultfinal;
+		double capacitanceDifference = c2-c1;
+		 System.out.println("The wires' change in capacity when moved from a distance of "
+		 		+   + initialDistance +
+		" m to " + finalDistance + "m is " + capacitanceDifference + " farads");
+	}
+}
